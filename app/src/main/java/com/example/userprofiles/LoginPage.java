b66d648f9cac20918d49a6fc62fa08de8bb44705
Amy Wang <amywang418@Amys-MacBook-Pro.local>
2022-02-22 18:12:55 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/userprofiles/LoginPage.java
@@ -0,0 +1,50 @@
+package com.example.userprofiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginPage extends AppCompatActivity {
+
+    public static final String KEY = "key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.signin);
+        final Button registerButton = findViewById(R.id.register);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                Intent intent = new Intent(LoginPage.this, ProfilePage.class);
+                String message;
+                message = username;
+
+                intent.putExtra(KEY, message);
+                startActivity(intent);
+
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameR = usernameEditText.getText().toString();
+                String passwordR = passwordEditText.getText().toString();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/userprofiles/ProfilePage.java
@@ -0,0 +1,38 @@
+package com.example.userprofiles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilePage extends AppCompatActivity {
+    public static final String KEY = "key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra((LoginPage.KEY));
+
+        TextView textView = findViewById(R.id.name);
+        textView.setText(message);
+        final Button mapButton = findViewById(R.id.map);
+
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ProfilePage.this, MapsActivity.class);
+
+                intent.putExtra(KEY, message);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
